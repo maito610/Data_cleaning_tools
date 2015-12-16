5065b5aa32a59a7a006d1c55883837cfda5439d7
maito610 <maito610+gh@gmail.com>
2015-12-16 15:09:10 +0900

    20151216_3

---
+++ get_zipcode_from_address/sample.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+__author__ = 'maito'
+__all__ = ['sample_function', ]
+
+
+# write your actual code here.
+
+def sample_function(first, second=4):
+    """This is a sample function to demonstrate doctests
+    of :mod:`get_zipcode_from_address.code` and docs.
+    It only return the sum of its two arguments.
+
+    Args:
+      :param first: (:class:`int`): first value to add
+      :param second:  (:class:`int`): second value to add, 4 by default
+
+    Returns:
+      * :class:`int`: the sum of `first` and `second`.
+
+    >>> sample_function(6, second=3)
+    9
+    >>> sample_function(6)
+    10
+    """
+    return first + second
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
