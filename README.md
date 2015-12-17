2a4b147fbac5a560fc40bd30cb0cbf49bcab8d6f
maito610 <maito610+gh@gmail.com>
2015-12-17 15:02:42 +0900

    20151216

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 Overview
 
-## get_zipcode_from_address
+## get_zipcode_from_address [in preparation]
 get Japanese zip code from address
 
 ### Description
--- get_zipcode_from_address/add2zip_list.py
+++ get_zipcode_from_address/add2zip_list.py
@@ -1,37 +1,78 @@
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+#  -*- coding: utf-8 -*-
 __author__ = 'maito'
 __all__ = ['add2zip_list']
 
+##########
 import sys
 import argparse
-from bs4 import BeautifulSoup
+import codecs
+import csv
 
-def __read_postal_csv(postal_file_path):
-    pass
-
-def add2zip_list(args):
+##########
+class Add2zip(object):
     """
-    >>> add2zip_list(args)
+    [CLASSES] Class説明文
     """
-    __read_postal_csv(args['postal_file_path'])
-    # address = args['address']
+
+    def parse_address(address):
+        return([])
+
+    def add_csv2list(address_list_file):
+        address_list = []
+        ff = codecs.open(address_list_file,'r','utf-8')
+        reader = csv.reader(ff)
+        for rr in reader:
+            address_list.append(rr)
+        return(address_list)
+
+    def zip_csv2dict(zipcode_list_file):
+        zipcode_list = []
+        ff = codecs.open(zipcode_list_file,'r','shift-jis')
+        reader = csv.reader(ff)
+        for rr in reader:
+            zipcode_list.append(rr)
+        return(zipcode_list)
+
+    def add2zip_zipcode(address, zipcode_dict):
+        """
+        [CLASSES] 関数説明文
+        Keyword arguments:
+        hoge -- 引数説明
+        """
+        # >>> add2zip_list(args)
+        pass
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument(
         '-a',
-        dest = 'address',
-        required = True,
+        dest = 'address_list_file',
+        nargs='?',
+        default='../data/address.csv',
         help='address to get zip code')
     parser.add_argument(
         '-f',
-        dest = 'postal_file_path',
+        dest = 'zipcode_list_file',
         nargs='?',
-        default='../data/KEN_ALL_ROME.CSV',
-        help='file path of postal code list KEN_ALL_ROME.CSV "http://www.post.japanpost.jp/zipcode/dl/roman-zip.html"')
+        default='../data/KEN_ALL.CSV',
+        help='file path of postal zip code list KEN_ALL.CSV "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"')
     args = vars(parser.parse_args())
-    add2zip_list(args)
+
+    address_list = Add2zip.add_csv2list(args['address_list_file'])
+    zipcode_dict = Add2zip.zip_csv2dict(args['zipcode_list_file'])
+
+    print(address_list[0])
+    print(zipcode_dict[0])
+
+    Add2zip.add2zip_zipcode('1',{})
+
+
+##########
+if __name__ == '__main__':
+    main()
+    # >>> Add2zip.parse_address('東京都港区芝浦4-4-27-501')
 
     import doctest
     doctest.testmod()
